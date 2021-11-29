d3801ef9c398c16f293641351bb620e928d1e607
Walkimist <77995652+Walkimist@users.noreply.github.com>
2021-11-29 19:30:43 -0300

    Add files via upload

---
+++ Lista 1 - Vetores/Atividade1Lista1.cpp
@@ -0,0 +1,19 @@
+//Paulo Vitor Bernardi Theobald
+//UC21103812
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+	double notas[30];
+	
+	for (int i = 0; i < 30; i ++) { //Pegar as notas
+		printf("Insira a nota do aluno(a) %i: ", i+1);
+		scanf("%lf", &notas[i]);
+		system("cls");
+	}
+	for (int i = 0; i < 30; i ++) { //Printar as notas
+		rintf("Nota do aluno(a) %i: %.1lf", i+1, notas[i]);
+		printf("\n");
+	}
+}
---
+++ Lista 1 - Vetores/Atividade3Lista1.cpp
@@ -0,0 +1,20 @@
+//Paulo Vitor Bernardi Theobald
+//UC21103812
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+	int numeros[100];
+	int resultado = 0;
+
+	for (int i = 0; i < 100; i ++) { //Pegar os numeros
+		printf("Insira o %io numero: ", i+1);
+		scanf("%i", &numeros[i]);
+		system("cls");
+	}
+	for (int i = 0; i < 100; i ++) { //Somar os numeros
+		resultado += numeros[i];
+	}
+	printf("A soma de todos os numeros e: %i", resultado); //Printar o resultado
+}
